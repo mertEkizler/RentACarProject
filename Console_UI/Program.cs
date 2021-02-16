17c34781d335318fc540920f9dd2bf00d5c207a8
mert ekizler <mertekizler@hotmail.com>
2021-02-16 18:20:02 +0300

    Add project files.

---
+++ Console_UI/Program.cs
@@ -0,0 +1,67 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace Console_UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAll();
+            //Add();
+            //Delete();
+            //Update();
+            //GetById();
+        }
+
+        //private static void GetById()
+        //{
+        //    CarManager carManager = new CarManager(new InMemoryCarDal());
+        //    foreach (var item4 in carManager.GetById(3))
+        //    {
+        //        Console.WriteLine(item4.CarName);
+        //    }
+        //}
+
+        //private static void Update()
+        //{
+        //    CarManager carManager = new CarManager(new InMemoryCarDal());
+        //    carManager.Update(new Car { CarName = "Doğan", BrandId = 2, CarId = 1, ColorId = 2, DailyPrice = 40, Description = "benzin", ModelYear = 1992 });
+        //    foreach (var item3 in carManager.GetAll())
+        //    {
+        //        Console.WriteLine(item3.CarId + " " + item3.CarName + " " + item3.BrandId + " " + item3.ColorId + " " + item3.DailyPrice + " " + item3.Description + " " + item3.ModelYear);
+        //    }
+        //}
+
+        //private static void Delete()
+        //{
+        //    CarManager carManager = new CarManager(new InMemoryCarDal());
+        //    carManager.Delete(new Car { CarId = 3 });
+        //    foreach (var item2 in carManager.GetAll())
+        //    {
+        //        Console.WriteLine(item2.CarName);
+        //    }
+        //}
+
+        //private static void Add()
+        //{
+        //    CarManager carManager = new CarManager(new InMemoryCarDal());
+        //    carManager.Add(new Car { CarName = "Şahin", BrandId = 2, CarId = 3, ColorId = 4, DailyPrice = 30, Description = "tüplü", ModelYear = 1993 });
+        //    foreach (var item in carManager.GetAll())
+        //    {
+        //        Console.WriteLine(item.CarName);
+        //    }
+        //}
+
+        //private static void GetAll()
+        //{
+        //    CarManager carManager = new CarManager(new InMemoryCarDal());
+        //    foreach (var cars in carManager.GetAll())
+        //    {
+        //        Console.WriteLine(cars.CarId + " " + cars.CarName + " " + cars.BrandId + " " + cars.ColorId + " " + cars.DailyPrice + " " + cars.Description + " " + cars.ModelYear);
+        //    }
+        //}
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,76 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+               new Car { CarId=1, CarName ="Honda", BrandId=1, ColorId =1, DailyPrice=50, Description="Benzin", ModelYear=2017},
+               new Car { CarId=2, CarName ="Mercedes", BrandId=2, ColorId =2, DailyPrice=85, Description="Dizel", ModelYear=2015},
+               new Car { CarId=3, CarName ="Hyundai", BrandId=3, ColorId =2, DailyPrice=60, Description="Gaz", ModelYear=2011},
+               new Car { CarId=4, CarName ="BMW", BrandId=4, ColorId =3, DailyPrice=70, Description="Hybrid", ModelYear=2014},
+               new Car { CarId=5, CarName ="Seat", BrandId=5, ColorId =3, DailyPrice=40, Description="Benzin-Gaz", ModelYear=2020}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.CarId == carId).ToList();
+        }
+
+        public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarName = car.CarName;
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
